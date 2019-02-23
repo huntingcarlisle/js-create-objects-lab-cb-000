984066fa04f22841eb2dd7ed2698798452d8bae9
Hunter Carlisle <huntingcarlisle@gmail.com>
2019-02-23 19:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function Dog(name, breed, age) {
   this.breed = breed;
   this.age = age;
 }
+
+function Cat(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret(name, age, foodRequirements) {
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
